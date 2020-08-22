cd03f35f2064acbc6cfbcbd5cf053f9fd7777fef
woshi555bin <783158350@qq.com>
2020-08-22 09:09:24 +0000

    Deploying to gh-pages from  @ c9de02478195a46fddd33b4ae1daf0cfecd76c64 🚀

---
+++ precache-manifest.94a9f290d9153f6b6a298d0b3a851734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a976bca9a2641cd111fc77f71262b0b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.02066a185f263e442578.bundle.js"
+  },
+  {
+    "url": "runtime~main.02066a185f263e442578.bundle.js"
+  },
+  {
+    "revision": "5cdd035647db29116585",
+    "url": "static/css/main.cc6f50a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.02066a185f263e442578.bundle.js"
+  },
+  {
+    "url": "vendors~main.02066a185f263e442578.bundle.js.LICENSE.txt"
+  }
+]);
